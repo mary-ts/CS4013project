a0d8d8c88a4fe58bebe0a9ccb4c3457c30c72311
Shtonks <58079793+Shtonks@users.noreply.github.com>
2020-12-07 17:21:32 +0000

    Add files via upload

---
+++ PropertyOwner.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class PropertyOwner{
+    ArrayList<Property> properties = new ArrayList<>();
+    private String owner;
+
+    public PropertyOwner(String owner){
+        this.owner = owner;
+    }
+
+    //fixed Double and Boolean issue here, but not in property-call group
+    public void registerProperty(String address, String eircode, String location, double marketValue, boolean privateResidence){
+        properties.add(new Property(this.owner, address, eircode, location, marketValue, privateResidence));
+    }
+
+    public String viewProperties(){
+        String propertyTotal = "";
+        for(Property n : properties){
+            propertyTotal += n.toString();
+        }
+        return propertyTotal;
+    }
+}
