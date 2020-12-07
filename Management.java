3c6dee59dea254d77f9cbe8c635adaae09212f99
rachycat <58234819+rachycat@users.noreply.github.com>
2020-12-07 21:21:54 +0000

    Update Management.java

--- Management.java
+++ Management.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Management {
 	//Get payments for property (array list in property)
+	private Payment payments;
+	public ArrayList<Integer> getProperty() {
+		payments = new Payment();
+		ArrayList<Integer> property = payments.getPayments();
+		return property;
+	}
 	//get payments for property owner
 }
