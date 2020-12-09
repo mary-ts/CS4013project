bef30b52135be455eb5ea8d9d9a6871501141f28
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2020-12-09 13:55:43 +0000

    Update TaxCalculator.java

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -1,14 +1,32 @@
 public class TaxCalculator {
 
+	public double totalTax, fixedCost, locationTax;
+	private static double[] value = {150000, 400000, 650000};
+	
+	private static String[] locations = {"City","Large Town", "Small Town", "Village", "Countryside"}; 
+	private static int[] locationVals = {100, 80, 60, 50, 25};
 	
 	//Will probably have 2 constructors- 1 default and 1 custom //
 	public TaxCalculator() {
 		
+			}
+   
+	public double getMarketTax(Property p) {
+		
+	}
+	
+	public double getLocationTax(Property p) {
+		p.getLocation();
+		for(int i=0; i < locations.length; i++) {
+			if(p.equals(locations[i])) {
+				locationTax = locationVals[i];
+			}
+		} return locationTax;
 	}
 
-	// will have getters for all info//
 	
+	public double calculateTotalTax() {
+		return totalTax;
+	}
 	
-	
-	//Will have the actual calculator method- will use the getter methods//
 }
