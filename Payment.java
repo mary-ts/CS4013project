147fdef2407713d42be91f73a089dbdca0eab59a
Shtonks <58079793+Shtonks@users.noreply.github.com>
2020-12-12 17:29:49 +0000

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -1,54 +1,36 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-
 public class Payment {
-    TaxCalculator taxCalc;
-    private double payment;
-    private double tax;
-    private double balance;
-    
     private int year;
+    private boolean isTaxPaid;
+    private double tax;
+    private double overdue;
 
-    public Payment(Property property, double amount){
-        taxCalc = new TaxCalculator();
-        this.year = (LocalDate.now()).getYear();
-        payment = amount;
-        setTax();
-        setPayment(amount);
-        makePayment();
-    }
-
-    public void setPayment(double amount){
-        payment = amount;
-    }
-
-    public double getPayment(){
-        return payment;
-    }
-
-    public void setTax(){
-        tax = taxCalc.getTotalTax();
-    }
-
-    public double getTax(){
-        return tax;
-    }
-
-    public void makePayment(){
-        balance = tax - payment;
-    }
-
-    public double getBalance() {
-        return balance;
+    public Payment(int year, double tax, boolean isTaxPaid, double overdue){
+        this.year = year;
+        this.isTaxPaid = isTaxPaid;
+        this.tax = tax;
+        this.overdue = overdue;
     }
 
     public int getYear() {
         return year;
     }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean getIsTaxPaid() {
+        return isTaxPaid;
+    }
+
+    public void setIsTaxPaid(boolean isTaxPaid) {
+        this.isTaxPaid = isTaxPaid;
+    }
+
+
     public String toString(){
-        return year + " Tax due: €" + tax + "   Amount paid: €" + payment + "   Left to pay: €"
-                + balance + "\n";
+        return year + " Tax due: €" + tax + "   Tax paid: €" + isTaxPaid + "   Overdue: €"
+                + overdue + "\n";
     }
 
 }
