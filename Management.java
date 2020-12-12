c2605309d407a9146e3d5d303f25c40c36a0c5bf
rachycat <58234819+rachycat@users.noreply.github.com>
2020-12-12 05:25:51 +0000

    Update Management.java

--- Management.java
+++ Management.java
@@ -2,11 +2,25 @@ import java.util.ArrayList;
 
 public class Management {
 	//Get payments for property (array list in property)
+	//specify property
 	private Payment payments;
-	public ArrayList<Integer> getProperty() {
+	private PropertyOwner property;
+	public ArrayList<Payment> getProperty(Property p) {
 		payments = new Payment();
-		ArrayList<Integer> property = payments.getPayments();
-		return property;
+		ArrayList<Payment> payments = p.getPaymentRecord();
+		return payments;
 	}
 	//get payments for property owner
-}
+	//specify owner
+/*	
+	public ArrayList<PropertyOwner> getOwnerProperty(PropertyOwner p) {
+		ArrayList<PropertyOwner> property = p.viewProperties();
+		return property;
+	}
+*/
+	
+	public String getOwnerProperty(PropertyOwner p) {
+		//String property = 
+				return p.viewProperties();
+		//return property;
+	}	
