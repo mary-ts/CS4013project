2d0b0bb8367f5e7c5c50215a6aa9ee91d6f49b55
Tamara Lawlor <55025022+TTLawlor@users.noreply.github.com>
2020-12-13 02:57:49 +0000

    Update Management.java

--- Management.java
+++ Management.java
@@ -23,8 +23,6 @@ public class Management {
                 System.out.println(Arrays.toString(tmp));
             }
         }
-
-        return ;
     }
 
     public void getPropertyTaxFromProperty(String eircode) {
@@ -44,7 +42,9 @@ public class Management {
             if (tmp[index].trim().equals(eircode))
                 System.out.println(Arrays.toString(tmp));
         }
-	    public void getTotalTax(String eircode){
+    }
+
+    private double getTotalTax(String eircode){
         String[] split = eircode.split(" ");
         String routingKey = split[0];
         double totalTax = 0;
@@ -70,10 +70,10 @@ public class Management {
                 totalTax = totalTax + Double.parseDouble(tmp[taxIndex]);
             }
         }
-        System.out.println(totalTax);
+        return totalTax;
     }
 
-    public void getAverageTax(String eircode){
+    private double getAverageTax(String eircode){
         String[] split = eircode.split(" ");
         String routingKey = split[0];
         double averageTax = 0;
@@ -89,7 +89,6 @@ public class Management {
             }
             if (headings[i].trim().equalsIgnoreCase("Tax")) {
                 taxIndex = i;
-                break;
             }
         }
 
@@ -102,5 +101,72 @@ public class Management {
             }
         }
         averageTax = averageTax/numProps;
-        System.out.println(averageTax);
+        return averageTax;
     }
+
+    private double getNumberPaid(String eircode){
+        String[] split = eircode.split(" ");
+        String routingKey = split[0];
+
+        ArrayList data = CSV.readCSVFile("Payments.csv");
+        String[] headings = (String[]) data.get(0);
+
+        int index = 0;
+        int paidIndex = 0;
+        for (int i = 0; i < headings.length; i++) {
+            if (headings[i].trim().equalsIgnoreCase("Eircode")) {
+                index = i;
+            }
+            if (headings[i].trim().equalsIgnoreCase("Paid")) {
+                paidIndex = i;
+            }
+        }
+
+        int numProps = 0;
+        for (int i = 0; i < data.size(); i++) {
+            String[] tmp = (String[]) data.get(i);
+            if (tmp[index].trim().contains(routingKey) && tmp[paidIndex].trim().equalsIgnoreCase("true")) {
+                numProps ++;
+            }
+        }
+        return numProps;
+    }
+
+    private double getPercentPaid(String eircode){
+        String[] split = eircode.split(" ");
+        String routingKey = split[0];
+        double propNumber = getNumberPaid(eircode);
+
+        ArrayList data = CSV.readCSVFile("Payments.csv");
+        String[] headings = (String[]) data.get(0);
+
+        int index = 0;
+        for (int i = 0; i < headings.length; i++) {
+            if (headings[i].trim().equalsIgnoreCase("Eircode")) {
+                index = i;
+            }
+        }
+
+        int numProps = 0;
+        for (int i = 0; i < data.size(); i++) {
+            String[] tmp = (String[]) data.get(i);
+            if (tmp[index].trim().contains(routingKey)) {
+                numProps ++;
+            }
+        }
+        return (propNumber/numProps)*100;
+    }
+
+    public void getTaxStatistics(String eircode){
+        String s = "Tax Statistics(based on routing key):\n";
+        double totalTax = getTotalTax(eircode);
+        double avgTax = getAverageTax(eircode);
+        double numPaid = getNumberPaid(eircode);
+        double percent = getPercentPaid(eircode);
+        s = s + "Total tax: " + totalTax +
+                "\nAverage tax: " + avgTax +
+                "\nNumber of Paid Taxes: " + numPaid +
+                "\nPercent of Paid Taxes: " + percent + "%";
+        System.out.println(s);
+    }
+}
