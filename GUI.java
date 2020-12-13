a1230b97953a2b203ec23cdab22c5c09960cfa61
Shtonks <58079793+Shtonks@users.noreply.github.com>
2020-12-13 16:28:16 +0000

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -11,6 +11,7 @@ public class GUI implements ActionListener {
     private JLabel lblPersonType, lblOwnerName;
     private JTextField txtOwnerName;
     private JButton btnOwnerLogin, btnManagement, btnBack;
+    private JScrollPane vertScroll, horzScroll;
 
 
 
@@ -66,23 +67,30 @@ public class GUI implements ActionListener {
     private JLabel lblWelcome, lblQueryYear, lblQueryProp;
     private JTextField txtQueryYear, txtQueryProp;
     private JButton btnRegisterProp, btnViewProperty, btnQueryYear, btnQueryProp;
+    private JTextArea display;
 
     private void ownerInit() {
         p = new PropertyOwner(name);
 
         panelOwner = new JPanel();
-        panelOwner.setLayout(new GridLayout(3,3,1,1));
+        //panelOwner.setLayout(new GridLayout(3,3,1,1));
 
         lblWelcome = new JLabel("Welcome");
         lblQueryYear = new JLabel("Query by year:");
-        lblQueryProp = new JLabel("Query by property");
-        txtQueryYear = new JTextField();
-        txtQueryProp = new JTextField();
+        lblQueryProp = new JLabel("Query by property(eircode)");
+        txtQueryYear = new JTextField(5);
+        txtQueryProp = new JTextField(10);
         btnRegisterProp = new JButton("Register Property");
         btnViewProperty = new JButton("View Properties");
         btnQueryYear = new JButton("Search");
         btnQueryProp = new JButton("Search");
-        //btnBack
+        display = new JTextArea(10, 15);
+        /*
+        vertScroll = new JScrollPane(display);
+        vertScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        horzScroll = new JScrollPane(display);
+        horzScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+         */
 
         btnRegisterProp.addActionListener(this);
         btnViewProperty.addActionListener(this);
@@ -98,6 +106,9 @@ public class GUI implements ActionListener {
         panelOwner.add(lblQueryProp);
         panelOwner.add(txtQueryProp);
         panelOwner.add(btnQueryProp);
+        panelOwner.add(display);
+        //panelOwner.add(vertScroll);
+        //panelOwner.add(horzScroll);
 
         frame.remove(panelIdentity);
         frame.setContentPane(panelOwner);
@@ -123,8 +134,10 @@ public class GUI implements ActionListener {
         txtMarketValue = new JTextField(10);
         txtPrivateResidence = new JTextField(5);
         btnActuallyRegProp = new JButton("Register");
+        btnBack = new JButton("Back");
 
         btnActuallyRegProp.addActionListener(this);
+        btnBack.addActionListener(this);
 
         panelReg.add(lblAddress);
         panelReg.add(txtAddress);
@@ -141,6 +154,7 @@ public class GUI implements ActionListener {
         panelReg.add(lblPrivateResidence);
         panelReg.add(txtPrivateResidence);
         panelReg.add(btnActuallyRegProp);
+        panelReg.add(btnBack);
 
         frame.remove(panelOwner);
         frame.setContentPane(panelReg);
@@ -163,6 +177,21 @@ public class GUI implements ActionListener {
             double marketVal = Double.parseDouble(txtMarketValue.getText());
             boolean priv = Boolean.parseBoolean(txtPrivateResidence.getText());
             p.registerProperty(address, eircode, location, marketVal, priv);
+        }
+        if(e.getSource() == btnBack){
+            frame.remove(panelReg);
+            frame.setContentPane(panelOwner);
+        }
+        if(e.getSource() == btnViewProperty){
+            display.setText(p.viewProperties());
+        }
+        if(e.getSource() == btnQueryYear){
+            int year = Integer.parseInt(txtQueryYear.getText());
+            display.setText(p.specificQuery(year));
+        }
+        if(e.getSource() == btnQueryProp){
+            String eircode = txtQueryProp.getText();
+            display.setText(p.specificQuery(eircode));
         }
         frame.validate();
         frame.repaint();
