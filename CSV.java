bd67a7e9c26630ee4458d6e8c8d4d4683787cc02
Tamara Lawlor <55025022+TTLawlor@users.noreply.github.com>
2020-12-13 15:44:26 +0000

    Update CSV.java

--- CSV.java
+++ CSV.java
@@ -5,22 +5,31 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class CSV {
-public static ArrayList<String[]> readCSVFile(String file) throws Exception{
+public static ArrayList<String[]> readCSVFile(String file){
     String[] line;
     ArrayList<String[]> list = new ArrayList<>();
-        CSVReader csvReader = new CSVReaderBuilder(new FileReader(file)).build();
+    try {
         CSVParser csvBuilder = new CSVParserBuilder().build();
+        CSVReader csvReader = new CSVReaderBuilder(new FileReader(file)).withCSVParser(csvBuilder).build();
 
-        while ((line = csvReader.readNext()) != null){
+        while ((line = csvReader.readNext()) != null) {
             list.add(line);
         }
         csvReader.close();
+    } catch (Exception e){
+        e.printStackTrace();
+    }
+
     return list;
 }
 
-public static void writeCSVFile(String file, String[] data) throws Exception {
+public static void writeCSVFile(String file, String[] data){
+    try {
         CSVWriter csvWriter = new CSVWriter(new FileWriter(file, true));
         csvWriter.writeNext(data, false);
         csvWriter.close();
+    }catch (Exception e){
+        e.printStackTrace();
+    }
 }
 }
